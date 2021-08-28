8705853fbcde5b4400b04f5294acde972f4e6fe8
Luis Osorio <luisfosg@gmail.com>
2021-08-28 16:43:53 -0500

    add eslint

--- part2/index.js
+++ part2/index.js
@@ -1,33 +1,39 @@
-import express from 'express';
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 app.get('/api/notes', (req, res) => {
-  res.send('Notes');
-});
+  res.send('Notes')
+})
 
 app.get('/api/notes/:id', (req, res) => {
-  const { id } = req.params;
-  res.send(`Note with id ${id}`);
-});
+  const { id } = req.params
+  res.send(`Note with id ${id}`)
+})
 
-app.delete('/api/notes/:id', (req, res)=> {
-  const { id } = req.params;
-  res.send(`Note with id ${id} deleted`);
-});
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params
+  res.send(`Note with id ${id} deleted`)
+})
 
 app.post('/api/notes', (req, res) => {
-  const { title, content } = req.body;
-  res.send(`Note with title ${title} and content ${content} created`);
+  const { title, content } = req.body
+  res.send(`Note with title ${title} and content ${content} created`)
+})
+
+app.use((req, res, next) => {
+  res.status(404).send('404 Not Found')
 })
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
+  console.log('Example app listening on port 3000!')
+})
