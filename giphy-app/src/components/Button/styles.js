63c64a3f89a89597101a56f97332077353895aef
LuisFOsG <luisfosgdev@gmail.com>
2021-10-11 19:15:27 -0500

    feat: create ThemeProvider in emotion

--- giphy-app/src/components/Button/styles.js
+++ giphy-app/src/components/Button/styles.js
@@ -5,14 +5,14 @@ export const Link = styled(LinkWouter)`
   text-decoration: none;
   border: 1px solid transparent;
   color: #fff;
-  background-color: #333435;
+  background-color: ${({theme}) => theme.colors.secondary};
   cursor: pointer;
   font-size: 1rem;
   padding: .5rem 1rem;
   margin-right: .5rem;
 
   :hover {
-    background-color: var(--brand-color_2);
+    background-color: #282c34;
   }
 `;
 
--- giphy-app/src/styles/index.js
+++ giphy-app/src/styles/index.js
@@ -2,3 +2,10 @@ export const bps = {
   xs: '@media (min-width: 600px)',
   sm: '@media (min-width: 800px)',
 }
+
+export const theme = {
+  colors: {
+    primary: '#0070f3',
+    secondary: '#333435',
+  }
+}
